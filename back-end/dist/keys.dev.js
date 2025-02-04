16695822a49c2f43227dd6d357b750cb080bd183
david sanchez <197341243+davidsanchez9936@users.noreply.github.com>
2025-02-04 17:10:42 -0500

    deleted key

--- back-end/dist/keys.dev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-require("dotenv").config();
-
-var mongoURL = "mongodb+srv://davidsanchez9936:".concat(process.env.MONGODB, "@eccomerce.rs9prel.mongodb.net/");
-module.exports = {
-  mongoURL: mongoURL
-};
-/* mongodb+srv://davidsanchez9936:hFJcqql16oTnervh@eccomerce.rs9prel.mongodb.net/ */
