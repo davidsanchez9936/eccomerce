6f6b87cda1eb849de7841fe3c56d097bf25c3cc9
david sanchez <197341243+davidsanchez9936@users.noreply.github.com>
2025-02-04 20:38:22 -0500

    second commit

--- back-end/firebase/index.js
+++ back-end/firebase/index.js
@@ -1,9 +1,19 @@
-var admin = require("firebase-admin");
+/* var admin = require("firebase-admin");
 
 var serviceAccount = require("../config/fbServiceAccountKey.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
+});
+
+module.exports = admin; */
+
+const admin = require("firebase-admin");
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
 });
 
 module.exports = admin;
