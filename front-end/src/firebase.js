999da2f4696c3f3e1418172fc01be2e5b4012516
david sanchez <197341243+davidsanchez9936@users.noreply.github.com>
2025-02-06 17:28:58 -0500

    firebase

--- front-end/src/firebase.js
+++ front-end/src/firebase.js
@@ -7,15 +7,16 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
+/* const firebaseConfig = {
   apiKey: "AIzaSyAQoDC2FkL3B9R5hp900SyfD10CF1ZKOYA",
   authDomain: "eccomerce-e2b86.firebaseapp.com",
   projectId: "eccomerce-e2b86",
   storageBucket: "eccomerce-e2b86.appspot.com",
   messagingSenderId: "908384704213",
   appId: "1:908384704213:web:4a6bf73988403c9aaa345c"
-};
+}; */
+
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(import.meta.env.VITE_APP_FIREBASECONFIG);
 export const auth = getAuth(app);
